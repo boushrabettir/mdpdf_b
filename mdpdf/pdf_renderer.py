bf561168167eba6d3a5d29679b634b5779ac8b51
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-06-26 23:05:45 -0700

    Update pdf_renderer.py

--- mdpdf/pdf_renderer.py
+++ mdpdf/pdf_renderer.py
@@ -18,7 +18,7 @@ from .headfoot import Header, Footer
 fontSize = 60  # choose font size of text
 headingfontSizes = [18, 16, 14, 12, 10, 10, 10, 10]
 lineheight = fontSize * 1.2  # line height is 20% larger
-margin = 30
+margin = 72
 
 # For images, we are looking for a pattern in the alt text,
 # e.g. Some text { width =31% }
